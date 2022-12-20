69600d8bd6f6cdd8a391e91ed0b8fd57f69d5b22
reifier-k <119746462+reifier-k@users.noreply.github.com>
2022-12-21 05:48:29 +0900

    不要な入力待機を削除

--- run-frontend.sh
+++ run-frontend.sh
@@ -4,12 +4,3 @@ cd `dirname $0`
 
 docker-compose exec frontend npm run build 
 docker-compose exec frontend npm run start
-
-while true
-do
-  echo "終了するにはexitと入力してください。"
-  read EXIT_CONFIRM
-  if [ $EXIT_CONFIRM == "exit" ]; then
-    break
-  fi
-done
