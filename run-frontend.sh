f89e412fb565ac54884b4a12b48ef8201d85a1bd
reifier-k <119746462+reifier-k@users.noreply.github.com>
2022-12-21 05:48:05 +0900

    frontendはproductionビルドを利用するように変更

--- run-frontend.sh
+++ run-frontend.sh
@@ -2,4 +2,14 @@
 
 cd `dirname $0`
 
-docker-compose exec frontend npm run dev
+docker-compose exec frontend npm run build 
+docker-compose exec frontend npm run start
+
+while true
+do
+  echo "終了するにはexitと入力してください。"
+  read EXIT_CONFIRM
+  if [ $EXIT_CONFIRM == "exit" ]; then
+    break
+  fi
+done
